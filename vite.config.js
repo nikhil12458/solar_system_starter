fbc889e70ba4318704da4d037b8a2c1ec0838e5f
Nikhil <ektak144@gmail.com>
2025-08-09 14:03:26 +0530

    v2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
-export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-}
+// export default {
+//     root: 'src/',
+//     publicDir: '../static/',
+//     base: './',
+// }
+
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src",
+  publicDir: "../static",
+  base: "./",
+  build: {
+    outDir: "../dist", // put dist outside src
+    emptyOutDir: true, // clean dist before build
+  },
+});
